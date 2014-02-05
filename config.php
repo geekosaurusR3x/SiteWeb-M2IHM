c745f8166624251bb88bfc81479cb2b1144c9ed1
Philipp Schmitt <philipp@schmitt.co>
2014-02-05 19:51:35 +0100

    Change theme

--- config.php
+++ config.php
@@ -5,7 +5,9 @@
 
 $config['site_title'] = 'Pico';			// Site title
 $config['base_url'] = ''; 				// Override base URL (e.g. http://example.com)
-$config['theme'] = 'default'; 			// Set the theme (defaults to "default")
+*/
+$config['theme'] = 'pico-flatui-blog'; 			// Set the theme (defaults to "default")
+/*
 $config['date_format'] = 'jS M Y';		// Set the PHP date format
 $config['twig_config'] = array(			// Twig settings
 	'cache' => false,					// To enable Twig caching change this to CACHE_DIR
